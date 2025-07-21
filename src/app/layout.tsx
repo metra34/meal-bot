169c683d9fbd373aa662f4f03a84594bbf7fdf63
Artem Clement <artemc@raidian.ai>
2025-07-21 16:40:05 -0400

    - use prisma instead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body>
+      <body cz-shortcut-listen="true">
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
